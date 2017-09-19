4d1eeb0134f14918f4a7fd0b1e65799aee24a3a0
Stefano Salati <stef.salati@gmail.com>
2017-09-19 10:58:18 +0200

    first (badly) working version

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,9 @@
 #include "kalman_filter.h"
+#include "tools.h"
+
+// STE
+#include <iostream>
+using namespace std;
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -18,22 +23,60 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
+  
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  
+  VectorXd z_pred = H_ * x_;  
+  VectorXd y = z - z_pred; 
+  MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+	
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+  
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  
+  Tools tools;
+    
+  //recover state parameters
+	float px = x_(0);
+	float py = x_(1);
+	float vx = x_(2);
+	float vy = x_(3);
+
+	//pre-compute a set of terms to avoid repeated calculation
+	float c1 = px*px+py*py;
+	float c2 = sqrt(c1);
+	
+	//compute h(x')	
+	VectorXd z_pred = VectorXd(3);
+	z_pred << c2, atan2(py,px), (px*vx+py*vy)/c2;   // TODO Controllare risultati di atan
+	
+	cout << "RADAR: passato da qui 1" << endl;
+  
+  VectorXd y = z - z_pred;
+  MatrixXd Hj_ = tools.CalculateJacobian(x_);
+  MatrixXd Hjt = Hj_.transpose();
+	MatrixXd S = Hj_ * P_ * Hjt + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Hjt;
+	MatrixXd K = PHt * Si;
+	
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * Hj_) * P_;
+  
 }
